22968aafab2967dd29dbd9f05448f616cb1b8903
Hamza <hamza.2simou@gmail.com>
2026-04-01 00:21:43 +0100

    add app

--- src/main/java/org/example/smartstock/App.java
+++ src/main/java/org/example/smartstock/App.java
@@ -1,5 +1,16 @@
 package org.example.smartstock;
 
 public class App {
-    p
+    public static void main(String[] args){
+       ProductRepository repository = new InMemoryProductRepository();
+       InventoryService service = new InventoryService(repository);
+
+        service.registerProduct("LAP-001", "Laptop", 10);
+        service.addStock("LAP-001", 5);
+        service.removeStock("LAP-001", 3);
+
+        Product product = service.getProduct("LAP-001");
+        System.out.println(product.getName() + " -> "+ product.getQuantity());
+
+    }
 }
